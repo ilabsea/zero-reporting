74ad6ba8e55a61f1346dbb90e0c9a15681af55b3
Channa Ly <channa.info@gmail.com>
2015-05-07 10:45:21 +0700

    add travis ci and codeclimate

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,3 +48,6 @@ RSpec.configure do |config|
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
 end
+
+require "codeclimate-test-reporter"
+CodeClimate::TestReporter.start
