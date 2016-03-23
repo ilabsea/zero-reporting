d1f2f92f3c836ffe5193a35328fc355f4a9bde15
thyda_eng <thyda@instedd.org>
2016-03-23 14:08:48 +0700

    fixes script add recipient in external sms setting

--- app/assets/javascripts/external_sms_settings/form.js
+++ app/assets/javascripts/external_sms_settings/form.js
@@ -5,16 +5,14 @@ $(function(){
 
 function addRecipient(){
   $('#btn-add-recipient').click(function(){
-    newRecipient = $('#recipient').val();
-    $('#list-recipients').append("<li><span class='rounded'>"+newRecipient+"</span><input type='hidden' name='external_sms_setting[recipients][]' value='"+newRecipient+"' />"+
-    " <a href='#'><span class='glyphicon glyphicon-remove-circle btn-remove-recipient' data-value='"+newRecipient+"'></span></a></li>");
+    recipient = $('#recipient');
+    newRecipient = recipient.val();
+    $('#list-recipients').append("<li id='recipient-"+newRecipient+"'><span class='rounded'>"+newRecipient+"</span><input type='hidden' name='external_sms_setting[recipients][]' value='"+newRecipient+"' />"+
+    " <a href='#' onclick='removeRecipient("+newRecipient+")' class='btn-remove-recipient'><span class='glyphicon glyphicon-remove-circle' data-value='"+newRecipient+"'></span></a></li>");
+    recipient.val('');
   });
 }
 
-function removeRecipient(){
-  $('.btn-remove-recipient').click(function(){
-    console.log('removeRecipient');
-    recipient = $(this).data("value");
-    $("#recipient-"+recipient).remove();
-  });
+function removeRecipient(recipient){
+  $("#recipient-"+recipient).remove();
 }
