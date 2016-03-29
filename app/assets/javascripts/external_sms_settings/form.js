ff7e80538d8a36c0466877c1f9170bffbc9e509c
thyda_eng <thyda@instedd.org>
2016-03-29 08:31:19 +0700

    trim phone number in external sms form

--- app/assets/javascripts/external_sms_settings/form.js
+++ app/assets/javascripts/external_sms_settings/form.js
@@ -6,7 +6,7 @@ $(function(){
 function addRecipient(){
   $('#btn-add-recipient').click(function(){
     recipient = $('#recipient');
-    newRecipient = recipient.val();
+    newRecipient = recipient.val().trim();
     if(newRecipient != ""){
       $('#list-recipients').append("<li id='recipient-"+newRecipient+"'><span class='rounded'>"+newRecipient+"</span><input type='hidden' name='external_sms_setting[recipients][]' value='"+newRecipient+"' />"+
       " <a href='#' onclick='removeRecipient("+newRecipient+")' class='btn-remove-recipient'><span class='glyphicon glyphicon-remove-circle' data-value='"+newRecipient+"'></span></a></li>");
