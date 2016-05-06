00b238f831ac840c00f3942dc370904023216d00
kakada <kakada.chheang@gmail.com>
2016-05-06 10:32:23 +0700

    fixes add/remove dynamic number for verboice sms notification

--- app/assets/javascripts/external_sms_settings/form.js
+++ app/assets/javascripts/external_sms_settings/form.js
@@ -5,16 +5,18 @@ $(function(){
 
 function addRecipient(){
   $('#btn-add-recipient').click(function(){
-    recipient = $('#recipient');
-    newRecipient = recipient.val().trim();
+    var recipient = $('#recipient');
+    var newRecipient = recipient.val();
     if(newRecipient != ""){
       $('#list-recipients').append("<li id='recipient-"+newRecipient+"'><span class='rounded'>"+newRecipient+"</span><input type='hidden' name='external_sms_setting[recipients][]' value='"+newRecipient+"' />"+
-      " <a href='#' onclick='removeRecipient("+newRecipient+")' class='btn-remove-recipient'><span class='glyphicon glyphicon-remove-circle' data-value='"+newRecipient+"'></span></a></li>");
+      " <a href='#' class='btn-remove-recipient'><span class='glyphicon glyphicon-remove-circle' data-value='"+newRecipient+"'></span></a></li>");
       recipient.val('');
     }
   });
 }
 
 function removeRecipient(recipient){
-  $("#recipient-"+recipient).remove();
+  $(document.body).on('click', ".btn-remove-recipient", function() {
+    $(this).parent().remove()
+  });
 }
