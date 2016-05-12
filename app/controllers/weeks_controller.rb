95288bfc58f34e6bedc6ab052439f630ca9ca3ad
kakada <kakada.chheang@gmail.com>
2016-05-12 11:47:16 +0700

    fixes ignore root for weeks serializer

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -2,6 +2,6 @@ class WeeksController < ApplicationController
   def index
     @year = params[:year].present? ? Calendar::Year.new(params[:year].to_i) : Calendar::Year.new(Time.now.year)
     @weeks = @year.available_weeks.map { |w| { id: w.week_number, display: w.display(Calendar::Week::DISPLAY_ADVANCED_MODE) } }
-    render json: @weeks
+    render json: @weeks, root: false
   end
 end
