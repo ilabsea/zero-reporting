2606f79f88fb2960a1ad4f7dd051926433c362cc
kakada <kakada.chheang@gmail.com>
2016-09-14 10:25:14 +0700

    Fixes not_before date time in VoiceAlertAdapter spec

--- spec/models/adapter/voice_alert_adapter_spec.rb
+++ spec/models/adapter/voice_alert_adapter_spec.rb
@@ -11,6 +11,9 @@ RSpec.describe Adapter::VoiceAlertAdapter, type: :model do
       let(:report_setting) { Setting::ReportSetting.new({ enables: ['voice'], x_week: 2, recipient_types: ['HC'] }) }
 
       before(:each) do
+        date = Date.new(2016, 9, 10)
+        allow(Date).to receive(:today).and_return(date)
+
         report_setting.templates = { 'voice' => { channel_id: 1, call_flow_id: 1, call_time: '08:00' } }
         alert = Alerts::VoiceAlert.new hc, report_setting
         @adapter = Adapter::VoiceAlertAdapter.new(alert)
