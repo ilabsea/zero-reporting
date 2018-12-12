468640379d1eba02d6bfce675b66376301d96870
Sokly <hengsokly23@gmail.com>
2018-12-12 23:47:55 +0700

    add verboice_name to report document

--- app/models/concerns/reports/elasticsearch.rb
+++ app/models/concerns/reports/elasticsearch.rb
@@ -11,11 +11,24 @@ module Reports::Elasticsearch
     document_type Settings.elasticsearch.document_type
 
     def as_indexed_json(options={})
-      self.as_json(
+      option = self.as_json(
         include: { user: {only: :username},
                    place: {only: [:name, :code, :kind_of]},
-                   report_variables: {only: [:variable_id, :type, :value]}
                  })
+
+      option['report_variables'] = []
+
+      self.report_variables.includes(:variable).each do |report_variable|
+        option['report_variables'].push(
+          {
+            verboice_name: report_variable.variable.verboice_name,
+            type: report_variable.type,
+            value: report_variable.value
+          }
+        )
+      end
+
+      option
     end
   end
 end
